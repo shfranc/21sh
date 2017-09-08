f206950b202134e879c1c2af96253bd9db331ce5
Sarah FRANC <sfranc@e2r7p17.42.fr>
2017-09-08 16:48:42 +0200

    exec wip expansion

--- srcs/exec/expansion.c
+++ srcs/exec/expansion.c
@@ -6,7 +6,7 @@
 /*   By: sfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/07 18:03:31 by sfranc            #+#    #+#             */
-/*   Updated: 2017/09/07 19:50:52 by sfranc           ###   ########.fr       */
+/*   Updated: 2017/09/08 16:48:15 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,92 @@
 
 static int	ft_dollar_isquoted(char *begin, char *dollar)
 {
+	int		escape;
 	char	*before;
 
+	ft_putendl("1");
 	if (dollar != begin && *(dollar - 1) == '\\')
 		return (1);
 	before = begin;
+	ft_putendl("2");
 	while (*before && before < dollar)
 	{
-		if (*before == '\'' && (before - 1) && *(before - 1) != '\\')
+		ft_putendl("3");
+		if (*before == '\\')
+			escape = ESCAPE;
+		if (!escape && *before == '\'')
 		{
 			before = before + ft_goto_next_quote(before, *before);
+			ft_putendl(before);
 			if (before > dollar)
 				return (1);
 		}
-		else if (*before == '"' && (before - 1) && *(before - 1) != '\\')
+		else if (!escape && *before == '"')
 		{
+			ft_putendl("4");
 			before = before + ft_goto_next_quote_withescape(before, *before);
+			ft_putendl("5");
+			ft_putendl(before);
 			if (before > dollar)
 				return (1);
 		}
-		before++;
+		else
+		{
+			before++;
+			escape = (escape & ESCAPE) ? escape ^ ESCAPE : 0;
+		}
 	}
 	return (0);
+}
+
+static void	ft_replace_dollar(char **str, char *dollar)
+{
+	char	*begin;
+	char	*end;
+	char	*var;
+	char	*replace;
+	char	*tmp;
+
+	begin = ft_strsub(*str, 0, dollar - *str);
+	ft_putstr("begin : ");
+	ft_putendl(begin);
+	var = NULL;
+	++dollar;
+	while (*dollar && ft_isalnum(*dollar))
+		var = ft_charappend(var, *dollar++);
+
+	ft_putstr("var a chercher : ");
+	ft_putendl(var);
+
+	replace = NULL;
+	if (var)
+		replace = ft_get_env_variable(g_env, var);
+
+	ft_putstr("replace : ");
+	ft_putendl(replace);
+
+	end = ft_strsub(*str, ft_strlen(begin) + ft_strlen(var) + 1, ft_strlen(dollar));
+	ft_putstr("end : ");
+	ft_putendl(end);
+
+	if (replace)
+	{
+		tmp = ft_strjoin3(begin, replace, end);
+		free(*str);
+		*str = tmp;
+	}
+	else
+	{
+		tmp = ft_strjoin(begin, end);
+		free(*str);
+		*str = tmp;
+	}
 }
 
 // ~ expansion : ne remplace que le 1er ~ si pas de ~ a la suite. (~~ n'est pas expandu)
 // PATH:expand tout du long
 
-void	ft_expand(t_token *token)
+void		ft_expand(t_token *token)
 {
 	t_token *tmp;
 	char	*dollar;
@@ -51,11 +109,15 @@ void	ft_expand(t_token *token)
 	{
 		if (tmp->token_type == WORD)
 		{
-			if ((dollar = ft_strchr(tmp->str, '$'))\
+			while ((dollar = ft_strchr(tmp->str, '$'))\
 					&& !ft_dollar_isquoted(tmp->str, dollar))
-				ft_putendl("replace $");
-			else
-				ft_putendl("ne rien faire");
+			{
+				ft_putstr("replace $ :");
+				ft_putendl(dollar);
+				ft_replace_dollar(&tmp->str, dollar);
+				ft_putstr("RESULT :");
+				ft_putendl(tmp->str);
+			}
 		}
 		tmp = tmp->next;
 	}
