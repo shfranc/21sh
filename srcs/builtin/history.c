f0da90a048482150b85296aa99a9f48f2790220d
Sarah FRANC <sfranc@e2r8p15.42.fr>
2017-09-21 17:53:53 +0200

    UPDATE option -c pour delete l'history

--- srcs/builtin/history.c
+++ srcs/builtin/history.c
@@ -6,7 +6,7 @@
 /*   By: sfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/21 12:08:12 by sfranc            #+#    #+#             */
-/*   Updated: 2017/09/21 15:23:26 by sfranc           ###   ########.fr       */
+/*   Updated: 2017/09/21 17:53:28 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,24 @@ int		ft_builtin_history(char **cmd)
 	int len;
 	int	i;
 
-	(void)cmd;
-	if (!g_shell->history)
-		ft_putendl(STR_HIST_EMPTY);
-	else
+	if (!cmd[1])
 	{
-		len = ft_tablen(g_shell->history);
-		i = 0;
-		while (i < len)
+		if (!g_shell->history)
+			ft_putendl(STR_HIST_EMPTY);
+		else
 		{
-			ft_putnbr(i + 1);
-			ft_putstr("\t");
-			ft_putendl(g_shell->history[i]);
-			i++;
+			len = ft_tablen(g_shell->history);
+			i = 0;
+			while (i < len)
+			{
+				ft_putnbr(i + 1);
+				ft_putstr("\t");
+				ft_putendl(g_shell->history[i]);
+				i++;
+			}
 		}
 	}
+	else if (ft_strequ(cmd[1], "-c"))
+		ft_freetab(&g_shell->history);
 	return (EXIT_SUCCESS);
 }
